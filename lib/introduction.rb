4ee83ebdcd08b1e3f713fea78f19f28d3e1525f4
Ian McKenzie <imckenzie777@gmail.com>
2020-04-20 18:30:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,13 @@ def  introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am leanring to program in #{language}"
+  puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
+
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional(, "Ruby")
 
 
 
